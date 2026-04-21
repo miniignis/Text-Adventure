de4b72f128ffb40f304dda499cbfbe92a21d47bd
miniignis <nieto.aidan.1@gmail.com>
2026-04-21 15:43:58 -0500

    Add classes

---
+++ main.py
@@ -0,0 +1,9 @@
+def getAction(prompt):
+    response = input(prompt + "\n")
+    return response.lower() # Return response is lowercase to make things easier.  
+
+
+
+# Game Loop
+while True:
+    pass
